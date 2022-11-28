896c39e5c9d15caa3b28359aa80c6e20f1b7e165
jameserv <106282058+jameserv@users.noreply.github.com>
2022-11-27 19:37:28 -0500

    Add app.js

---
+++ src/chapter02/chapter02PracticeExercises/practiceExercise2-3/app.js
@@ -0,0 +1,4 @@
+let sideA = prompt("Enter the length of Side A:\n");
+let sideB = prompt("Enter the length of Side B:\n");
+let hypotenuse = Math.sqrt(sideA ** 2 + sideB ** 2);
+console.log(hypotenuse);
